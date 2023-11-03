730e326b2c398c58498c4385c5fc73bcb6a4aa09
kxplxn <aakaplan@proton.me>
2023-11-03 20:07:14 +0000

    Exercise files are copied over and structured in accordance with the course content.

---
+++ 02-structural/01-adapter/sammysangAdapter.go
@@ -0,0 +1,37 @@
+package main
+
+type sammysangAdapter struct {
+	// TODO: add a field for the SammysangTV reference
+}
+
+func (ss *sammysangAdapter) turnOn() {
+	// TODO
+}
+
+func (ss *sammysangAdapter) turnOff() {
+	// TODO
+}
+
+func (ss *sammysangAdapter) volumeUp() int {
+	// TODO
+	return -1
+}
+
+func (ss *sammysangAdapter) volumeDown() int {
+	// TODO
+	return -1
+}
+
+func (ss *sammysangAdapter) channelUp() int {
+	// TODO
+	return -1
+}
+
+func (ss *sammysangAdapter) channelDown() int {
+	// TODO
+	return -1
+}
+
+func (ss *sammysangAdapter) goToChannel(ch int) {
+	// TODO
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kxplxn/go-design-patterns
+
+go 1.21.3
